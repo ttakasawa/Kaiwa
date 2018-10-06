cea3eece6b47df05c9fe2fc4572b6d1eccc575d2
TT <tom_takasawa@icloud.com>
2018-10-06 18:27:06 -0400

    Kaiwa initiating

---
+++ Kaiwa/Kaiwa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kaiwa
+//
+//  Created by Tomoki Takasawa on 10/6/18.
+//  Copyright © 2018 Tomoki Takasawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
