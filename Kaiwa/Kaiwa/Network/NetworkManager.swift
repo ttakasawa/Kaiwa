daaf75e061a1124229df85252cfad3d2d26c65ec
Tomoki William Takasawa <>
2018-10-06 22:56:01 -0400

    UserNetwork, NetworkManager

---
+++ Kaiwa/Kaiwa/Network/NetworkManager.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkManager.swift
+//  Kaiwa
+//
+//  Created by Tomoki William Takasawa on 10/6/18.
+//  Copyright © 2018 Tomoki Takasawa. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkManager {
+    
+}
+
+
+
